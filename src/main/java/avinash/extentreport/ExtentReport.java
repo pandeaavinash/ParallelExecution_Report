8c38560b7dbac9cf8ab30d2fc8ddf38fa703559a
avinash <aavinashpande@gmail.com>
2022-02-21 18:19:17 +0530

    Updated ExtentReport file

--- src/main/java/avinash/extentreport/ExtentReport.java
+++ src/main/java/avinash/extentreport/ExtentReport.java
@@ -2,6 +2,7 @@ package avinash.extentreport;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentReport 
 {
@@ -15,6 +16,9 @@ public class ExtentReport
 		
 		reporter.config().setReportName("Avinash Automation Report");
 		reporter.config().setDocumentTitle("Avinash Document");
+		reporter.config().enableTimeline(true);
+		reporter.config().setTheme(Theme.DARK);
+		reporter.config().setCSS(".black-text { color: #fff !important; }");
 		
 		extent = new ExtentReports();
 		extent.attachReporter(reporter);
